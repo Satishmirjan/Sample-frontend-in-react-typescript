23723c328996be16b8b8b41c67d34b3449a11740
satishmirjan <satishnda3576@gmail.com>
2025-11-03 10:25:07 +0530

    readme is updated

---
+++ backend/temp.java
@@ -0,0 +1,10 @@
+class temp{
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    int i=0;
+    while(i<5){
+      System.out.println("i is: " + i);
+      i++;
+    }
+  }
+}
